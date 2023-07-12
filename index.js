2fee6b566504cf4b432f95586eca81083abf273c
Dario Olivares <olivaresdario2001@gmail.com>
2023-07-12 18:51:25 -0400

    cuarta modificacion de archivo de conexion para cyclic

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 9001;
+const port = process.env.PORT || 3000;
 
 // Configurar la conexión a MongoDB Atlas
 
