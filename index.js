8ce83dda7618e9cb7e5f9295716065dd55e8a934
Dario Olivares <olivaresdario2001@gmail.com>
2023-07-12 18:31:52 -0400

    modificacion para conexion desde cyclic

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const userRoutes = require('./src/routes/userRoutes');
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+
+const app = express();
+const port = 9001;
 
 // Configurar la conexión a MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI, {
+
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try
+  {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Mongo Connected : ${conn.connection.host}`);
+  }
+  catch(error){
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+
+/*mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => console.log('Conexión exitosa a MongoDB Atlas'))
   .catch((error) => console.log('Error al conectar a MongoDB Atlas:', error.message));
+*/
 
-const app = express();
 app.use(express.json());
 app.use(cors());
 //llama a los enrutadores de usuarios-------------
@@ -21,7 +39,9 @@ app.use('/api/users', userRoutes);
 //------------------------------
 
 // Puerto en el que se ejecutará el servidor
-const port = 9001;
-app.listen(port, () => {
-  console.log(`Servidor iniciado en http://localhost:${port}`);
+
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Servidor iniciado en http://localhost:${port}`);
+  })
 });
